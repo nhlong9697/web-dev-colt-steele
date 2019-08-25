a82c337deb6163e2c4cd9e1b3c8296d2bd8cd1e3
Long Nguyen <n.h.long.9697@gmail.com>
2019-08-25 18:20:29 +0700

    draft

--- To-do-list-project/js/todoList.js
+++ To-do-list-project/js/todoList.js
@@ -1,3 +1,22 @@
-$("li").click(function () { 
+$("ul").on("click","li", function () { 
   $(this).toggleClass("completed")
 });
+
+$("ul").on("click","span", function (event) { 
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) { 
+  if (event.which === 13) {
+    var newTodo = $(this).val();
+    console.log("enter is ok"); 
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newTodo + "</li>"); 
+  }
+});
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
+})
